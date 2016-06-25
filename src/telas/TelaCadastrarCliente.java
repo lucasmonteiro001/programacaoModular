f9fdb26835eb2e5dcdfdee869f5062223ee9a773
iagoadvaz <iagoadvaz@gmail.com>
2016-06-25 17:39:19 -0300

    Update TelaCadastrarCliente.java

--- src/telas/TelaCadastrarCliente.java
+++ src/telas/TelaCadastrarCliente.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import javax.naming.spi.DirStateFactory;
 
 /**
